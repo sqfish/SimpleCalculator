e063dc21b454e52954623e38a56af8ed763c6c7c
Sally Fisher <sqfisher@gmail.com>
2015-11-19 09:19:17 -0600

    Parsing single digit positive numbers

---
+++ Calculator/Parse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Parse
+    {
+        public string ParsedData;
+        public string[] Terms;
+        public char Operator;
+        public void ParseInput(string input)
+        {
+            string output = input.ToLower();
+            Regex whitespace = new Regex(@"\s");
+            output = whitespace.Replace(output, "");
+            ParsedData = output;
+            Terms = new string[] { output.Substring(0, 1), output.Substring(2, 1) };
+            Operator = output[1];
+        }
+    }
+}
---
+++ CalculatorTests/ParseTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class ParseTest
+    {
+        [TestMethod]
+        public void EnsureICanCreateAnInstance()
+        {
+            Parse aParse = new Parse();
+            Assert.IsNotNull(aParse);
+        }
+
+        [TestMethod]
+        public void EnsureICanRemoveWhiteSpace()
+        {
+            Parse aParse = new Parse();
+            aParse.ParseInput("8          2     23210 7");
+            string expected = "82232107";
+            Assert.AreEqual(expected, aParse.ParsedData);
+        }
+
+        [TestMethod]
+        public void EnsureICanExtractInputTerms()
+        {
+            // Arrange
+            Parse aParse = new Parse();
+            int firstTerm = 1;
+            int secondTerm = 8;
+            // Act
+            aParse.ParseInput("1 + 8");
+            //var parsedTerms = aParse.Terms;
+            Assert.AreEqual(firstTerm.ToString(), aParse.Terms[0]);
+            Assert.AreEqual(secondTerm.ToString(), aParse.Terms[1]);
+        }
+
+        [TestMethod]
+        public void EnsureICanCreateAnInstance2()
+        {
+            Parse aParse1 = new Parse();
+            Assert.IsNotNull(aParse1);
+        }
+    }
+}
